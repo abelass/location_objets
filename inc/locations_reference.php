62116aca60326472bea7033f90e2c637de64ff24
abelass <rainer@websimple.be>
2018-05-31 14:19:23 +0200

    doc

--- inc/locations_reference.php
+++ inc/locations_reference.php
@@ -13,22 +13,22 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
+ * Pioché dans Commandes.
  * Génère un numéro unique utilisé pour remplir le champ `reference` lors de la création d'une location.
  *
- * Le numéro retourné est le nombre de secondes écoulées depuis le 1er janvier 1970
+ * Le numéro retourné est la date suivi de l'identifiant
  *
  * @example
  *     ```
  *     $fonction_reference = charger_fonction('locations_reference', 'inc/');
  *     ```
  *
- * @param int $id_commande
- * @param int $id_auteur
- *     (optionnel) identifiant de l'auteur
+ * @param int $id_objets_location
+
  * @return string
  *     reference de la commande
 **/
-function inc_locations_reference_dist($id_objets_location, $id_auteur=0){
+function inc_locations_reference_dist($id_objets_location){
 
 	if ($date = sql_getfetsel('date', 'spip_spip_objets_locations', '$id_objets_location='.intval($id_objets_location))) {
 		$t = strtotime($date);
