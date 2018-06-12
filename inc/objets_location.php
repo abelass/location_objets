4863d0665e2488dccbd29e2b6d4e4fe4fbad507f
abelass <rainer@websimple.be>
2018-06-12 11:46:25 +0200

    auto doc

--- inc/objets_location.php
+++ inc/objets_location.php
@@ -8,49 +8,46 @@
  * @licence    GNU/GPL v3
  * @package    SPIP\Location_objets\Fonctions
  */
-
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+/**
+ * Calcule le prix et infis relationnées d'un objet
+ *
+ * @param array $set
+ *        	Les valeurs à enregistrer.
+ * @param array $contexte
+ * 	        Les variables de l'environnement.
+ * @return array
+ * 	        Les valeurs à enregistrer.
+ */
 function location_prix_objet($set, $contexte) {
-	foreach($contexte as $cle => $valeur) {
+	foreach ($contexte as $cle => $valeur) {
 		$$cle = $valeur;
 	}
 
 	if (test_plugin_actif('prix_objets')) {
 		$prix_objet = TRUE;
-		if ($prix_unitaire_ht = prix_par_objet(
-			$location_objet,
-			$id_location_objet,
-			array(
+		if ($prix_unitaire_ht = prix_par_objet($location_objet, $id_location_objet, array(
+			'date_debut' => $date_debut,
+			'date_fin' => $date_fin
+		))) {
+			$set['prix_unitaire_ht'] = $prix_unitaire_ht;
+			$prix_ttc = prix_par_objet($location_objet, $id_location_objet, array(
 				'date_debut' => $date_debut,
-				'date_fin' => $date_fin,
-			)
-			)) {
-				$set['prix_unitaire_ht'] = $prix_unitaire_ht;
-				$prix_ttc = prix_par_objet(
-					$location_objet,
-					$id_location_objet,
-					array(
-						'date_debut' => $date_debut,
-						'date_fin' => $date_fin,
-					),
-					'prix'
-					);
-				$set['taxe'] = $prix_ttc - $prix_unitaire_ht;
-				$set['devise'] = devise_defaut_objet($id_location_objet, $location_objet);
-				$set['prix_total'] = _request('prix_total');
-			}
+				'date_fin' => $date_fin
+			), 'prix');
+			$set['taxe'] = $prix_ttc - $prix_unitaire_ht;
+			$set['devise'] = devise_defaut_objet($id_location_objet, $location_objet);
+			$set['prix_total'] = _request('prix_total');
+		}
 	}
 
-	$set = pipeline(
-		'location_prix_objet',
-		array(
-			'data' => $set,
-			'args' => $contexte
-		)
-	);
+	$set = pipeline('location_prix_objet', array(
+		'data' => $set,
+		'args' => $contexte
+	));
 
 	return $set;
 }
